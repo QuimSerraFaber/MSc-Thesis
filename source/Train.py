d6f7b53e738975fa0a8de90a30e744fa6693718c
QuimSerraFaber <quimserraf@gmail.com>
2024-01-17 00:36:29 +0100

    Your commit message here

--- source/Train.py
+++ source/Train.py
@@ -5,14 +5,14 @@ from Losses import MSE
 from Predicted_signal import predict_polynomial_signal
 
 # Initialize the models
-models = [FullyConnectedNN(in_features) for _ in range(6)]
+models = [FullyConnectedNN(in_features) for _ in range(4)]
 
 # Define the optimizer
 optimizers = [optim.Adam(model.parameters(), lr=0.001) for model in models]
 
 # Define training parameters
 num_epochs = 10
-batch_size = 64
+batch_size = 128
 
 # Define the train function
 
--- source/models/Initial_fc_nn.py
+++ source/models/Initial_fc_nn.py
@@ -6,11 +6,11 @@ import torch.optim as optim
 class FullyConnectedNN(nn.Module):
     def __init__(self, in_features):
         super(FullyConnectedNN, self).__init__()
-        self.fc1 = nn.Linear(in_features, 128)  # Adjust in_features to match your data
-        self.bn1 = nn.BatchNorm1d(128)
-        self.fc2 = nn.Linear(128, 256)
-        self.bn2 = nn.BatchNorm1d(256)
-        self.fc3 = nn.Linear(64, 1)  # Output layer
+        self.fc1 = nn.Linear(in_features, 64)  # Adjust in_features to match your data
+        self.bn1 = nn.BatchNorm1d(64)
+        self.fc2 = nn.Linear(64, 128)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.fc3 = nn.Linear(32, 1)  # Output layer
 
     def forward(self, x):
         x = torch.relu(self.bn1(self.fc1(x)))
